0a4b56f936ef3a1209360e599f06bd77cd8e7604
GabrielC0 <gabriel.changrenier@efrei.net>
2022-10-09 21:31:25 +0200

    update

--- src/Navbar/Sections/Contact/Contact.js
+++ src/Navbar/Sections/Contact/Contact.js
@@ -1,24 +1,27 @@
 import React from "react";
+import "./Form.css";
 
 export default function Contact() {
   return (
     <form name="contact" method="post">
       <input type="hidden" name="form-name" value="contact" />
-      <p>
+      <div>
         <label htmlFor="name">Name</label> <br />
         <input type="text" id="name" name="name" required />
-      </p>
-      <p>
+      </div>
+      <div>
         <label htmlFor="email">Email</label> <br />
         <input type="email" id="email" name="email" required />
-      </p>
-      <p>
+      </div>
+
+      <div>
         <label htmlFor="message">Message</label> <br />
         <textarea id="message" name="message" required></textarea>
-      </p>
-      <p>
+      </div>
+
+      <div>
         <input type="submit" value="Submit message" />
-      </p>
+      </div>
     </form>
   );
 }
