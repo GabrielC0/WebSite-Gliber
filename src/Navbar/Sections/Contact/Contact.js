b3b1d7f293bb7d1fb3dce72f582da04f7bb27d0d
GabrielC0 <gabriel.changrenier@efrei.net>
2022-10-09 17:04:24 +0200

    update

--- src/Navbar/Sections/Contact/Contact.js
+++ src/Navbar/Sections/Contact/Contact.js
@@ -13,32 +13,20 @@ function Contact() {
 
   return (
     <div className="body">
-      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message"></textarea>
-      </form>
+      <form onSubmit={handleSubmit(onSubmit)} method="post">
+        <input
+          type="text"
+          placeholder="Nom Prenom"
+          {...register("Nom Prenom", {})}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          {...register("Email", { required: true })}
+        />
+        <input type="text" placeholder="Message" {...register("Message", {})} />
 
-      <form method="post">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <input type="submit" />
       </form>
     </div>
   );
